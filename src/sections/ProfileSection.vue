233701b3d45f092a02f7b6ad4030c22ddc7a8468
BhismaAprian <gmdrose123@gmail.com>
2025-12-11 11:27:15 +0800

    Create ProfileSection.vue

---
+++ src/sections/ProfileSection.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="pt-32 pb-20 bg-white">
+    <div class="max-w-7xl mx-auto px-6 md:px-12 lg:px-20">
+      <div class="columns-3xs"></div>
+      <div class="grid grid-cols-1 lg:grid-cols-2 gap-16 items-center">
+        <div>
+          <h1
+            class="text-[#004080] text-4xl md:text-5xl lg:text-6xl text-center md:text-start lg:text-start font-extralight leading-[1.1] mb-8"
+          >
+            Bhisma Aprian<br />
+            Prayogi
+          </h1>
+          <p
+            class="text-[#E5A835] text-lg uppercase mb-4 text-center md:text-start lg:text-start leading-[1.1]"
+          >
+            11241018 - Informatika - Institut Teknologi Kalimantan
+          </p>
+          <p
+            class="text-[#004080]/60 text-base md:text-lg text-center lg:text-start font-light leading-[1.1] max-w-lg"
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec consectetur consectetur
+            ullamcorper. Fusce eget fringilla ipsum, eu vestibulum sem. Nam sit amet urna eget neque
+            auctor hendrerit. Ut ac massa nec orci sodales tristique quis in nisl. Ut ultrices
+            placerat malesuada. Integer commodo mattis ullamcorper.
+          </p>
+        </div>
+
+        <div class="invisible lg:visible md:visible flex justify-center lg:justify-end">
+          <div class="absolute -bottom-4 -right-4 w-48 h-48 rounded-full bg-[#004080]/5"></div>
+
+          <div class="relative bg-[#F3F4F6] p-16 rounded-2xl">
+            <img
+              src="https://avatars.githubusercontent.com/u/111761188?v=4"
+              alt="ITK Wear Logo"
+              class="w-40 h-auto"
+            />
+            <div class="mt-6 text-center"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
